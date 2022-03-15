2818312c3b8b51ed38af51b2f1244eff1066a0c3
Loli <lolimurillo5@gmail.com>
2022-03-15 16:11:43 +0100

    Express, ruta usuarios y estructuracion

--- README.md
+++
@@ -1,2 +0,0 @@
-# app-back
-backend app
---
+++ routes/user.routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/register", (req, res) =>
+    res.send("Este es el endpoint de registro")
+);
+
+module.exports = router;
